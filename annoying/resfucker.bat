e5623bb30bfc2997d465196f63db1899433c316f
Enginner <75962433+EnginnerDog@users.noreply.github.com>
2023-10-01 01:52:43 +0100

    Update resfucker.bat

--- annoying/resfucker.bat
+++ annoying/resfucker.bat
@@ -1,8 +1,18 @@
 @echo off
-echo ResFucker v1
-echo warning, it's fucking annoying
-pause
-:loop
-mode con: cols=800 lines=600
-mode con: cols=1024 lines=768
-goto loop
+
+REM Create a temporary VBScript file
+echo Set objWMIService = GetObject("winmgmts:\\.\root\cimv2") > temp.vbs
+echo Set colItems = objWMIService.ExecQuery("Select * from Win32_VideoController") >> temp.vbs
+echo For Each objItem in colItems >> temp.vbs
+echo objItem.SetCurrentHorizontalResolution 800 >> temp.vbs
+echo objItem.SetCurrentVerticalResolution 600 >> temp.vbs
+echo WScript.Sleep 3000 >> temp.vbs
+echo objItem.SetCurrentHorizontalResolution 1024 >> temp.vbs
+echo objItem.SetCurrentVerticalResolution 760 >> temp.vbs
+echo Next >> temp.vbs
+
+REM Run the temporary VBScript file
+cscript //nologo temp.vbs
+
+REM Delete the temporary VBScript file
+del temp.vbs
