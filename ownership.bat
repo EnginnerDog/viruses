17824843cec3974cdee24c3f5e43b763bc1afc19
Enginner <75962433+EnginnerDog@users.noreply.github.com>
2023-10-01 01:17:16 +0100

    Update ownership.bat

--- ownership.bat
+++ ownership.bat
@@ -1,5 +1,5 @@
 @echo off
+echo Forkie-Approved! Run as admin to take ownership of system folders.
 set /p file="Enter the path to the file you want to take ownership of: "
-
 takeown /f "%file%" /r /d y
 icacls "%file%" /grant Administrators:F /t
