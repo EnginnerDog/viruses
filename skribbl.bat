cb3093c63c349116743bd0cf69bf80d17d8bda1c
rok.avi <75962433+EnginnerDog@users.noreply.github.com>
2023-10-01 11:18:20 +0100

    Update skribbl.bat

--- skribbl.bat
+++ skribbl.bat
@@ -1,6 +1,7 @@
 @echo off
-echo by EnginnerDog and rok.avi
-echo this is FontFucker
+echo FontFucker
+echo by rok.avi and enginnerdog
+echo 10/10, forkie approved
 pause
 echo Checking admin privileges...
 net session >nul 2>&1
@@ -21,6 +22,12 @@ if %errorLevel% == 0 (
     reg add "HKCU\Control Panel\Desktop" /v "FontSmoothingGamma" /t REG_DWORD /d "800" /f
     reg add "HKCU\Control Panel\Desktop" /v "FontSmoothingContrast" /t REG_DWORD /d "1200" /f
     reg add "HKCU\Control Panel\Desktop" /v "FontSmoothingOrientation" /t REG_DWORD /d "1" /f
+    
+    reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Segoe UI (TrueType)" /t REG_SZ /d "comic.ttf" /f
+    reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Segoe UI Bold (TrueType)" /t REG_SZ /d "comicbd.ttf" /f
+    reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Segoe UI Italic (TrueType)" /t REG_SZ /d "comici.ttf" /f
+    reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Segoe UI Bold Italic (TrueType)" /t REG_SZ /d "comicbi.ttf" /f
+    
     taskkill /f /im explorer.exe
     timeout 2
     start explorer.exe
